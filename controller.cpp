ee04932f2098aefbe6b6fe5f336b225ae2578809
Adam Awad <awada@wit.edu>
2015-04-13 11:30:43 -0400

    Moved code into MVC structure.

--- controller.cpp
+++ controller.cpp
@@ -20,13 +20,21 @@ https://wiki.libsdl.org/SDL_Event
 void Controller::loop() {
     SDL_Event e;
     unsigned int lastTime = 0, currentTime;
-    std::map<SDL_Keycode, Direction> direction;
-    direction[SDLK_UP] = UP;
-    direction[SDLK_DOWN] = DOWN;
-    direction[SDLK_LEFT] = LEFT;
-    direction[SDLK_RIGHT] = RIGHT;
+    std::map<SDL_Keycode, char> letter;
+    letter[SDLK_a] = 'A';
+    letter[SDLK_b] = 'B';
+    letter[SDLK_c] = 'C';
+    letter[SDLK_d] = 'D';
+	// TODO: fill this in
+	SDL_Rect choice[3];
+	for (int i = 0; i < 3; i++) {
+		choice[i].x = 100 * i;
+		choice[i].y = 80;
+		choice[i].w = 100;
+		choice[i].h = 30;
+	}
 
-    while(!model->gameOver()) {
+    while(!model->gameOver() || model->state == NEW_GAME) {
         currentTime = SDL_GetTicks();
         // Do stuff here to animate as necessary
         view->show(model);
@@ -36,16 +44,23 @@ void Controller::loop() {
                 return;
             case SDL_KEYDOWN:
                 switch(e.key.keysym.sym) {
-                case SDLK_DOWN:
-                case SDLK_UP:
-                case SDLK_LEFT:
-                case SDLK_RIGHT:
-//                    model->go(direction[e.key.keysym.sym]);
+                    model->makeGuess(letter[e.key.keysym.sym]);
                 break;
                 default:
                 break;
                 }
             case SDL_MOUSEBUTTONDOWN:
+				if (model->state == NEW_GAME) {
+					SDL_Rect mouse;
+					mouse.x = e.button.x;
+					mouse.y = e.button.y;
+					for (int i = 0; i < 3; i++) {
+						if (SDL_HasIntersection(&mouse, &choice[i])) {
+							model->makeChoice(i);
+						}
+					}
+					// set the choice
+				}
                 break;
             }
         }
--- model.cpp
+++ model.cpp
@@ -1,17 +1,81 @@
 #include "model.h"
 #include <cstdlib>
 #include <ctime>
+#include <algorithm>
 
 using namespace std;
 
 // Constructor initializes the object
 Model::Model() {
+	// level options 
+	words[0].push_back("WORK");
+	words[0].push_back("PINK");
+	words[0].push_back("PEN");
+	words[0].push_back("TOE");
+	words[0].push_back("SHOE");
+	words[0].push_back("CAKE");
+	words[1].push_back("IPHONE");
+	words[1].push_back("POINT");
+	words[1].push_back("APPLE");
+	words[1].push_back("MARKER");
+	words[1].push_back("CANDLE");
+	words[1].push_back("CANADA");
+	words[2].push_back("VEHICLE");
+	words[2].push_back("PLASTIC");
+	words[2].push_back("PICTURE");
+	words[2].push_back("TELEVISION");
+	words[2].push_back("COMPUTER");
+	words[2].push_back("ORIGINAL");
+	makeChoice(0);
+	state = NEW_GAME;
 }
+
 // Destructor deletes dynamically allocated memory
 Model::~Model() {
 }
 
+// Start a new game
+void Model::makeChoice(int choice) {
+	vector<string> wordList = words[choice];
+	random_shuffle(wordList.begin(), wordList.end());
+	theword = wordList[0];
+	
+	progress = string(theword.size(), '-');          // word guessed so far
+	used = "";                            // letters already used
+	wrong = 0;
+	state = PLAYING;
+	lastGuess = '\0';
+}
+
+bool Model::guessed() {
+	return lastGuess != '\0';
+}
+
+// Did the last guess match the word?
+bool Model::matched()
+{
+	return (theword.find(lastGuess) != string::npos);
+}
+
+// Make a guess and update progress
+void Model::makeGuess(char letter) {
+	lastGuess = letter;
+	if (matched()) {
+		for (int i = 0; i < theword.length(); i++) {
+			if (theword[i] == lastGuess) {
+				progress[i] = lastGuess;
+			}
+		}
+	} else {
+		++wrong;
+	}
+}
+
+bool Model::lost() {
+	return wrong == MAX_WRONG;
+}
+
 bool Model::gameOver() {
-    return false;
+    return (wrong < MAX_WRONG) && (progress != theword);
 }
 
